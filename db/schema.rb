0e3f28da664976b39760efe29640ba3ef1a63196
vagrant <vagrant@localhost.localdomain>
2018-08-24 03:06:47 +0000

    modify schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_24_004749) do
+ActiveRecord::Schema.define(version: 2018_08_24_025945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "entries", force: :cascade do |t|
     t.string "title"
